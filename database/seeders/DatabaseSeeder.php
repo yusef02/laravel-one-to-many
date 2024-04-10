8e860d4a01c8858819d4cdb67b95aa129cdea002
yusef02 <youssef.bouziane2402@gmail.com>
2024-04-10 14:22:57 +0200

    update Type model and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             ProjectSeeder::class,
+            TypeSeeder::class,
         ]);
     }
 }
--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,12 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $types = config('types');
+
+        foreach ($types as $type) {
+            $new_type = new Type();
+            $new_type->type = $type;
+            $new_type->save();
+        }
     }
 }
